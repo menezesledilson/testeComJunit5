45de9482ade2c6669b3e157535399aa9434edac4
menezesledilson <ledilsonmenezes@gmail.com>
2024-05-27 16:14:03 -0300

    Classe user resource finById

--- src/main/java/br/rest/api/resources/UserResource.java
+++ src/main/java/br/rest/api/resources/UserResource.java
@@ -1,6 +1,8 @@
-package br.rest.api.domain.resources;
+package br.rest.api.resources;
 
 import br.rest.api.domain.User;
+import br.rest.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
+    @Autowired
+    private UserService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<User>findById(@PathVariable Integer id){
-return  ResponseEntity.ok().body(new User(1,"Valdir","val@email.com","123"));
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
